b7ce5629540d376a6c0c07404557c11817a323f8
Aman Kumar <meamank@gmail.com>
2019-11-15 19:41:06 +0530

    initial

---
+++ popup.js
@@ -0,0 +1,17 @@
+// Inject the payload.js script into the current tab after the popout has loaded
+window.addEventListener('load', function (evt) {
+	chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
+		file: 'payload.js'
+	});;
+});
+
+// Listen to messages from the payload.js script and write to popout.html and open new tab
+chrome.runtime.onMessage.addListener(function (message) {
+	document.getElementById('pagetitle').innerHTML = message;
+	console.log(message);
+	if (message == message.includes("videos")){
+		chrome.tabs.executeScript({
+		  file: 'redirect.js'
+		});
+	}
+});
